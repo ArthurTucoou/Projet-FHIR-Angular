49666ba78da86bfeea6e23d2ce94c14a0b909e24
Arthur Tucoou <arthur.tucoou@etud.univ-jfc.fr>
2025-05-22 08:38:33 +0200

    Suppression patient

--- src/app/components/patient-list/patient-list.component.ts
+++ src/app/components/patient-list/patient-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FhirService } from '../../services/fhir.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-patient-list',
+  imports: [MatIconModule],
   templateUrl: './patient-list.component.html',
   styleUrls: ['./patient-list.component.scss']
 })
@@ -12,23 +15,40 @@ export class PatientListComponent implements OnInit {
   errorMessage = '';
 
   private fhirService = inject(FhirService);
+  private snackBar = inject(MatSnackBar);
 
   ngOnInit(): void {
     console.log("tesstt");
-    
+
     this.fhirService.getAllPatients().subscribe({
       next: (response) => {
         console.log(response);
-        
+
         this.patients = response.entry?.map((e: any) => e.resource) || [];
         this.isLoading = false;
         console.log(this.isLoading);
-        
+
       },
       error: (error) => {
         this.errorMessage = 'Erreur lors du chargement des patients.';
         this.isLoading = false;
       }
     });
+  }
+
+  deletePatient(id: string) {
+    if (window.confirm('Voulez-vous vraiment supprimer ce patient ?')) {
+      this.fhirService.deletePatient(id).subscribe({
+        next: () => {
+          this.patients = this.patients.filter(p => p.id !== id); // Met à jour la liste localement
+          this.snackBar.open('Patient supprimé avec succès', 'Fermer', { duration: 3000 });
+        },
+        error: (error) => {
+          this.snackBar.open('Erreur lors de la suppression', 'Fermer', { duration: 3000 });
+          console.error(error);
+        }
+      });
+    }
+
   }
 }
